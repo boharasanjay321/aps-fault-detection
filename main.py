cc3312dcba99505a41cc1a334b345c2c1f6db741
sanjay <bohara.sanjay@gmail.com>
2022-12-01 11:55:15 +0530

    remove hyphen in main.py file

--- main.py
+++ main.py
@@ -2,10 +2,8 @@ import pymongo
 
 # Provide the mongodb localhost url to connect python to mongodb.
 client = pymongo.MongoClient("mongodb://localhost:27017/neurolabDB")
-
 # Database Name
 dataBase = client["neurolabDB"]
-
 # Collection  Name
 collection = dataBase['Products']
 
